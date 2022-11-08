48101f55d7ce32376cf52b8d411e0c41c64cbc2c
Lucio Anderlini <Lucio.Anderlini@cern.ch>
2022-11-08 14:50:08 +0100

    Added setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='ipynbc',
+    version='v0.1',
+    packages=['ipynbc'],
+    url='https://github.com/landerlini/ipynbc',
+    license='MIT',
+    author='Lucio Anderlini',
+    author_email='lucio.anderlini@fi.infn.it',
+    description='Simple script to convert python notebook with optional code only'
+)
